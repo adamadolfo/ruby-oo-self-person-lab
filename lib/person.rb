a5f0014c7305c831b26a3cd26f3ce2bf2ca06494
logicfirst <slee@logicfirstllc.com>
2020-08-27 17:12:59 -0400

    Success

--- lib/person.rb
+++ lib/person.rb
@@ -20,20 +20,50 @@ class Person
     end
 
     def happy?
-        if @happiness > 7
+        if self.happiness > 7
             return true
         else
             return false
         end
     end
     def clean?
-        if @hygiene > 7 
+        if self.hygiene > 7 
             return true
         else 
             return false
         end
     end
+
+    def get_paid(salary)
+        self.bank_account = salary + self.bank_account
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        #Self.hygiene=(self.hygiene - 3)
+        self.hygiene -=3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+
 end
 joe = Person.new("Joe")
 joe.happiness
+joe.take_bath
+bob = Person.new("Bob")
+bob.hygiene = 2
+bob.work_out
+
 
