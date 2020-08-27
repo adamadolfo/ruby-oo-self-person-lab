c1a61a977ab2c7eb1c43f9972c21e1a036efc6d9
Adam <adamadolfo8@gmail.com>
2020-08-27 15:48:37 -0400

    changing turns

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,39 @@
 # your code goes here
-#jfjf
+require 'pry'
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+    @happiness = happiness.clamp(0, 10)
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene.clamp(0, 10)
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+    def clean?
+        if @hygiene > 7 
+            return true
+        else 
+            return false
+        end
+    end
+end
+joe = Person.new("Joe")
+joe.happiness
+
