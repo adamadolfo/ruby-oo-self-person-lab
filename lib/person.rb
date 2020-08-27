663763ec901b3ae1a6852675c83f56dc6c27e588
tcharfi66 <charfitej6@gmail.com>
2020-08-27 17:26:20 -0400

    greatr success

--- lib/person.rb
+++ lib/person.rb
@@ -57,6 +57,24 @@ class Person
         return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
+    def start_conversation(person,topic)
+
+        if topic == "politics"
+            person.happiness  -= 2
+            self.happiness -= 2
+         
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            person.happiness  += 1
+            self.happiness += 1
+            return 'blah blah sun blah rain'
+        else 
+            return 'blah blah blah blah blah'
+        end
+
+       
+    end
+
 
 end
 joe = Person.new("Joe")
